d6e533902e261c59324fea87079103c3b5cb3b6e
Noah Johnson <jnoahj@byu.edu>
2021-02-17 14:20:23 -0700

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AthenaBookstore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/IBookstoreRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// Interface defining the Bookstore Repository that will be implemented in EFBookstore Repository
+
+namespace AthenaBookstore.Models
+{
+    public interface IBookstoreRepository
+    {
+        IQueryable<Book> Books { get; }
+    }
+}
