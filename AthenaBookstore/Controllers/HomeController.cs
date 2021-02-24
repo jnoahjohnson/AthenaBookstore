a1acb7c074ef002b562bdfbe4442158d52382635
Noah Johnson <jnoahjohnson@gmail.com>
2021-02-24 14:39:55 -0700

    Fixed directory

---
+++ AthenaBookstore/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using AthenaBookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AthenaBookstore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private IBookstoreRepository _repository;
+
+        public HomeController(ILogger<HomeController> logger, IBookstoreRepository repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            // Pass in the repository books
+            // Also, change this in the future back into the index, for the sake of the continued project I didn't want to change too much and then have to change it back
+            return View("ViewBooks", _repository.Books);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AthenaBookstore/Models/BookstoreDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// Default class to setup the context for the database on the client
+
+namespace AthenaBookstore.Models
+{
+    public class BookstoreDbContext : DbContext
+    {
+        public BookstoreDbContext (DbContextOptions<BookstoreDbContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
